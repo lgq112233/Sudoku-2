95223500aa567e617b99fdcf33ba2f3b7cfa10dd
Shuuichi <150706530@qq.com>
2018-03-24 00:07:46 +0800

    可生成终局

--- generator.cpp
+++ generator.cpp
@@ -5,14 +5,10 @@
 #include <cstdlib>
 using namespace std;
 
-int Board[9][9];
+extern int Board[9][9];
 int Try_List[9];
 
 int Random_Init(int Num[]){    //随机生成1-9全排列
-
-	srand((unsigned)time(NULL));    //时间种子
-	for (int i = 0; i < 10; i++)
-		printf(" %d/n", rand());
 
 	for (int i = 0; i < 9; i++)    //顺序填入1~9
 		Num[i] = i + 1;
@@ -33,8 +29,8 @@ bool Judge(int x,int y,int num){
 			return 0;
 	}
 	int area_x = x - x % 3, area_y = y - y % 3;    //计算所处宫格左上角坐标
-	for (int i = area_x; i < 3; i++)    //当前宫格合法判断
-		for (int j = area_y; j < 3;j++)
+	for (int i = area_x; i < area_x+3; i++)    //当前宫格合法判断
+		for (int j = area_y; j < area_y+3;j++)
 			if (Board[i][j] == num)
 				return 0;
 
@@ -42,6 +38,7 @@ bool Judge(int x,int y,int num){
 }
 
 int Solve_Sudoku(int x,int y){
+	int ori = Board[x][y];    //记录初值
 	int next_x = x + (y + 1) / 9, next_y = (y + 1) % 9;    //下一方格坐标
 
 	if (x >= 9)    //全部完成
@@ -60,6 +57,7 @@ int Solve_Sudoku(int x,int y){
 			}
 		}
 	}
+	Board[x][y] = ori;    //回溯还原
 
 	return 0;
 }
@@ -67,8 +65,9 @@ int Solve_Sudoku(int x,int y){
 int Creat_Sudoku(int Sodoku_Num){
 
 	freopen("sudoku.txt", "w", stdout);
+	srand((unsigned)time(NULL));    //时间种子
 	while (Sodoku_Num--){
-		for (int i = 0; i < 9;i++)
+		for (int i = 0; i < 9;i++)    //清空宫格
 			memset(Board[i], 0, sizeof(Board[i]));
 		Random_Init(Board[0]);    //随机初始化第一行数字
 		for (int i = 0; i < 9; i++)
@@ -78,11 +77,12 @@ int Creat_Sudoku(int Sodoku_Num){
 			}
 		Random_Init(Try_List);    //随机初始化数字尝试顺序
 		Solve_Sudoku(1,0);    //求解残局
-		for (int i = 0; i < 9; i++)    //打印
+		for (int i = 0; i < 9; i++)   //打印
 			for (int j = 0; j < 9; j++)
 				printf("%d%c", Board[i][j], j == 8 ? '\n' : ' ');
 		putchar('\n');
 	}
+	fclose(stdout);
 
 	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -5,13 +5,52 @@
 #include <cstdlib>
 using namespace std;
 
+extern int Solve_Sudoku(int x,int y);
 extern int Creat_Sudoku(int Sodoku_Num);
+int Board[9][9];
 
-int Sudoku_Num;
+int read(char str[]){    //读取数字
+
+	int Num = 0,len=strlen(str);
+	for (int i = 0; i < len; i++){
+		if (str[i]>'9' || str[i] < '0')    //非数字
+			return 0;
+		Num *= 10;
+		Num += str[i] - '0';
+		if (Num > 1000000)    //数字过大
+			return 0;
+	}
+	return Num;
+}
 
 int main(int argc,char *argv[]){
-	if (strcmp(argv[1], "-c") == 0){
-		Sudoku_Num = argv[2][0] - '0';
-		Creat_Sudoku(Sudoku_Num);
+
+	if (strcmp(argv[1], "-c") == 0){    //生成终局命令
+		if (int Sudoku_Num = read(argv[2]))
+			Creat_Sudoku(Sudoku_Num);    //生成终局
+		else
+			printf("Please enter the number 1-1000000\n");    //异常处理
 	}
+	else if (strcmp(argv[1], "-s") == 0){
+		freopen(argv[2], "r", stdin);
+		freopen("sudoku.txt", "w", stdout);
+		int temp,i=0,j=0;
+		while (~scanf("%d", &temp)){
+			Board[i][j] = temp;
+			i += (j + 1) / 9;
+			j = (j + 1) % 9;
+			if (i == 9){    //求解数独
+				Solve_Sudoku(0, 0);
+				i = j = 0;
+				for (int i = 0; i < 9; i++)   //打印
+					for (int j = 0; j < 9; j++)
+						printf("%d%c", Board[i][j], j == 8 ? '\n' : ' ');
+				putchar('\n');
+			}
+		}
+		fclose(stdin);
+		fclose(stdout);
+	}
+
+	return 0;
 }
